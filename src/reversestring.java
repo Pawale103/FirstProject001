058e95aa389aab71b4543539607779b498e4e161
sudhakar pawale <sudhakar pawale@MSI>
2022-12-11 17:40:31 +0530

    reverse String program committed

--- src/reversestring.java
+++ src/reversestring.java
@@ -3,21 +3,33 @@ public class reversestring {
 
 	public static void main(String[] args) {
 		
+//		
+//		StringBuilder sb=new StringBuilder("this is rev sting");
+//		System.out.println(sb.reverse());
+//		
+//		//System.out.println(StringFormatter.revrseString());
+//		
+//		StringBuffer sb1 = new StringBuffer("pawale patil");
+//		System.out.println(sb1.reverse());
+//		
+//		String str="this is now string";
+//		
+//		StringBuilder sb11= new StringBuilder(str);
+//		
+//		StringBuilder rev=sb11.reverse();
+//		System.out.println(rev);     
+//		
 		
-		StringBuilder sb=new StringBuilder("this is rev sting");
-		System.out.println(sb.reverse());
+
+		String str="String to be reversed";
+		String rev="";
 		
-		//System.out.println(StringFormatter.revrseString());
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		System.out.println(rev);
 		
-		StringBuffer sb1 = new StringBuffer("pawale patil");
-		System.out.println(sb1.reverse());
-		
-		String str="this is now string";
-		
-		StringBuilder sb11= new StringBuilder(str);
-		
-		StringBuilder rev=sb11.reverse();
-		System.out.println(rev);          
 		
 		
 		
