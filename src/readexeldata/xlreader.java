39ad3449911b398d073e23e47041eecdfe504f9f
sudhakar pawale <sudhakar pawale@MSI>
2023-03-06 22:31:24 +0530

    New commit

--- src/readexeldata/xlreader.java
+++ src/readexeldata/xlreader.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class xlreader {
 
 	public static void main(String[] args) throws IOException {
-System.setProperty("webdriver.chrome.driver","C:\\Users/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users/chromedriver.exe");
 		
 		WebDriver driver = new ChromeDriver();
 		
